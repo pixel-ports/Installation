68ddd697c4f03befc35354f9034b3da935822c89
marc-despland <marc.despland@art122-5.net>
2020-05-26 09:01:05 +0200

    Adding frontrp configuration

---
+++ docker/public/frontrp/docker_entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+envsubst '$$NGINX_PORT $$NGINX_TLS_PORT $$WILMA_HOST $$KEYROCK_HOST $$DASHBOARD_WEB_HOST $$DASHBOARD_BACKEND_HOST $$PIXEL_DOMAIN' /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+rm -f /etc/nginx/conf.d/default.conf.template
+
+exec nginx -g 'daemon off;'
