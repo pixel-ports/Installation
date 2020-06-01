24b037bba02d944c682e7c1b2d4f30189a2558ea
marc-despland <marc.despland@art122-5.net>
2020-06-01 11:11:39 +0200

    iptables rules to allow DNS query

--- docker/public/pixel-rules.install.sh
+++ docker/public/pixel-rules.install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 PUBLIC_INTERFACE=ens2
 source .env
-PUBLIC_INTERFACE=$PUBLIC_INTERFACE PUBLIC_HOST_IP=$PUBLIC_HOST_IP envsubst '$$PUBLIC_INTERFACE $$PUBLIC_HOST_IP' < pixel-rules.template > pixel-rules
+PUBLIC_INTERFACE=$PUBLIC_INTERFACE PUBLIC_HOST_IP=$CORE_HOST_IP envsubst '$$PUBLIC_INTERFACE $$CORE_HOST_IP' < pixel-rules.template > pixel-rules
 chmod a=rx pixel-rules
