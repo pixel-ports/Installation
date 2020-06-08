0bb71bdeb7c40d1fc53986de2a3b4263a4c86065
Marc Despland <marc.despland@orange.com>
2020-06-08 08:10:03 +0000

    Syntax issues

--- docker/core/dashboard/configure.sh
+++ docker/core/dashboard/configure.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -o allexport
 source ../.env
+set +o allexport
 
-envsubst '$$PUBLIC_HOST_IP $$CORE_HOST_IP $$PIXEL_DOMAIN $$PIXEL_INTERNAL_DOMAIN' < ./pixel_ports/.env_production.template > ./pixel_ports/.env_production
+envsubst '$$PUBLIC_HOST_IP $$CORE_HOST_IP $$PIXEL_DOMAIN $$PIXEL_INTERNAL_DOMAIN' < ./pixel_ports/.env.production.template > ./pixel_ports/.env.production
