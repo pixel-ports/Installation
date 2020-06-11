91979e3f708e6af6f6340b05a5e35baab9db7d5d
root <marc.despland@orange.com>
2020-06-11 15:49:11 +0000

    syntax

--- docker/public/frontrp/docker_entrypoint.sh
+++ docker/public/frontrp/docker_entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-envsubst '$$NGINX_PORT $$NGINX_TLS_PORT $$WILMA_HOST $$KEYROCK_HOST $$DASHBOARD_WEB_HOST $$DASHBOARD_BACKEND_HOST $$PIXEL_DOMAIN $$PIXEL_INTERNAL_DOMAIN' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '$$NGINX_PORT $$NGINX_TLS_PORT $$WILMA_HOST $$KEYROCK_HOST $$DASHBOARD_WEB_HOST $$DASHBOARD_BACKEND_HOST $$PIXEL_DOMAIN $$PIXEL_INTERNAL_DOMAIN $$OPERATIONTOOLS_API_HOST' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 rm -f /etc/nginx/conf.d/default.conf.template
 
 exec nginx -g 'daemon off;'
