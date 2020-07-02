b9c15bbafe893d332c03062cb58c120cc2056cc4
marc-despland <marc.despland@art122-5.net>
2020-07-02 13:17:40 +0200

    missing KIBANA_HOST

--- docker/public/frontrp/docker_entrypoint.sh
+++ docker/public/frontrp/docker_entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-envsubst '$$NGINX_PORT $$NGINX_TLS_PORT $$WILMA_HOST $$KEYROCK_HOST $$DASHBOARD_WEB_HOST $$DASHBOARD_BACKEND_HOST $$PIXEL_DOMAIN $$PIXEL_INTERNAL_DOMAIN $$OPERATIONTOOLS_API_HOST' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '$$NGINX_PORT $$NGINX_TLS_PORT $$WILMA_HOST $$KEYROCK_HOST $$DASHBOARD_WEB_HOST $$DASHBOARD_BACKEND_HOST $$PIXEL_DOMAIN $$PIXEL_INTERNAL_DOMAIN $$OPERATIONTOOLS_API_HOST $$KIBANA_HOST' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 rm -f /etc/nginx/conf.d/default.conf.template
 /usr/bin/htpasswd -ci /etc/nginx/conf.d/admin.passwd pixel < ${PIXEL_ADMIN_PASSWORD_FILE}
 exec nginx -g 'daemon off;'
