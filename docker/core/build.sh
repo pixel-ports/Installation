b07b0489929789b0802aedd5d54ecd48b12a6274
root <root@sflnx3904.ced.adds>
2020-09-24 14:51:05 +0200

    solve syntaxe issues

--- docker/core/build.sh
+++ docker/core/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd dashboard; ./configure.sh
-cd ot-core; ./configure.sh
+cd dashboard; ./configure.sh; cd ..
+cd ot-core; ./configure.sh; cd ..
 docker-compose -f docker-compose.yaml -f docker-compose-elastic.yaml -f docker-compose-ih.yaml -f docker-compose-tools.yaml -f docker-compose-ot.yaml -f docker-compose-dashboard.yaml build
--- docker/core/ot-core/configure.sh
+++ docker/core/ot-core/configure.sh
@@ -4,5 +4,5 @@ set -o allexport
 source ../.env
 set +o allexport
 
-export OTDB_PASSWORD = `echo ../secrets/ot.db.password`
-envsubst '$$PUBLIC_HOST_IP $$CORE_HOST_IP $$PIXEL_DOMAIN $$PIXEL_INTERNAL_DOMAIN $$OTDB_PWSSWORD' < default.configuration.xml.template > default.configuration.xml
+export OTDB_PASSWORD=`cat ../secrets/ot.db.password`
+envsubst '$$PUBLIC_HOST_IP $$CORE_HOST_IP $$PIXEL_DOMAIN $$PIXEL_INTERNAL_DOMAIN $$OTDB_PASSWORD' < default.configuration.xml.template > default.configuration.xml
