6fd616af3a6a8062d28de1b22778bb506d1c778a
mdespland <marc.despland@orange.com>
2020-11-11 10:56:31 +0100

    change for insiel installation

---
+++ docker/core/update-image.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+docker pull pixelh2020/information-hub-orion-data-collector 
+docker pull pixelh2020/information-hub-controller 
+docker pull pixelh2020/information-hub-extractor 
+docker pull pixelh2020/information-hub-monitor 
+docker pull pixelh2020/information-hub-writer 
+docker pull pixelh2020/information-hub-elasticsearch-proxy 
+docker pull pixelh2020/information-hub-data-processor-orchestrator
