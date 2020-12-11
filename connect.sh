8e4d914653fe835ba4d0c3144588fbb5b8dcc135
mdespland <marc.despland@orange.com>
2020-12-11 13:48:11 +0100

    add missing -f docker-compose-security.yaml

--- connect.sh
+++ connect.sh
@@ -2,10 +2,10 @@
 keypass=/Users/marcdespland/Projects/work/Pixel
 case "$1" in
         core)
-            exec ssh -i $keypass/gpmb_private root@212.47.227.23
+            exec ssh -i $keypass/gpmb_private -p 66 root@212.47.227.23
             ;;
         public)
-            exec ssh -i $keypass/gpmb_private root@163.172.188.163
+            exec ssh -i $keypass/gpmb_private -p 66 root@163.172.188.163
             ;;
         public-nagios)
             ssh -i $keypass/gpmb_private -L 127.0.0.1:8088:10.12.182.193:8088 root@163.172.188.163
--- docker/core/update-datarepo.sh
+++ docker/core/update-datarepo.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker-compose -f docker-compose.yaml -f docker-compose-elastic.yaml -f docker-compose-ih.yaml -f docker-compose-tools.yaml -f docker-compose-ot.yaml -f docker-compose-dashboard.yaml build --no-cache datarepo
+docker-compose -f docker-compose.yaml -f docker-compose-security.yaml -f docker-compose-elastic.yaml -f docker-compose-ih.yaml -f docker-compose-tools.yaml -f docker-compose-ot.yaml -f docker-compose-dashboard.yaml build --no-cache datarepo
