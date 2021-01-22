409ad1624b05c92c4f154ccc3b5fed741926ee5b
mdespland <marc.despland@orange.com>
2021-01-22 09:38:23 +0100

    Provisioning 1.1.0 with dashboard

--- docker/core/dal-provisioning.sh
+++ docker/core/dal-provisioning.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker-compose -f docker-compose.yaml -f docker-compose-elastic.yaml -f docker-compose-ih.yaml -f docker-compose-tools.yaml -f docker-compose-ot.yaml  -f docker-compose-dal-provisioning.yaml up dal-provisioning
+docker-compose -f docker-compose.yaml -f docker-compose-security.yaml -f docker-compose-elastic.yaml -f docker-compose-ih.yaml -f docker-compose-tools.yaml -f docker-compose-ot.yaml  -f docker-compose-dashboard.yaml  -f docker-compose-dal-provisioning.yaml up dal-provisioning
