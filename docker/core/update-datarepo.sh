a544078e93e9f76f97cf1e7f2b757d3ddc84ff87
mdespland <marc.despland@orange.com>
2021-02-08 10:00:01 +0100

    syntax

--- docker/core/update-datarepo.sh
+++ docker/core/update-datarepo.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker-compose -f docker-compose.yaml -f docker-compose-security.yaml -f docker-compose-elastic.yaml -f docker-compose-ih.yaml -f docker-compose-tools.yaml -f docker-compose-ot.yaml -f docker-compose-dashboard.yaml build --no-cache datarepo
-docker-compose -f docker-compose.yaml -f docker-compose-security.yaml -f docker-compose-elastic.yaml -f docker-compose-ih.yaml -f docker-compose-tools.yaml -f docker-compose-ot.yaml -f docker-compose-dashboard.yaml install datarepo
+docker-compose -f docker-compose.yaml -f docker-compose-security.yaml -f docker-compose-elastic.yaml -f docker-compose-ih.yaml -f docker-compose-tools.yaml -f docker-compose-ot.yaml -f docker-compose-dashboard.yaml up -d datarepo
