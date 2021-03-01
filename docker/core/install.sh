7deaaa5cbe1e9b886614b410def4dead6d868302
mdespland <marc.despland@orange.com>
2021-03-01 16:10:59 +0100

    Add restart core_monitor_1 core_controller_1

--- docker/core/install.sh
+++ docker/core/install.sh
@@ -13,6 +13,8 @@ wait_for_orion() {
 }
 
 docker-compose -f docker-compose.yaml -f docker-compose-security.yaml -f docker-compose-elastic.yaml -f docker-compose-ih.yaml -f docker-compose-tools.yaml -f docker-compose-ot.yaml  -f docker-compose-dashboard.yaml up -d
+sleep 5
+docker restart core_monitor_1 core_controller_1
 
 curl -s -o /dev/null http://127.0.0.1:1026/version
 if [ $? -ne 0 ]; then
---
+++ docker/version.md
@@ -0,0 +1,2 @@
+* 2.0.0 : 01/01/2021 Initial Release
+* 2.0.1 : 01/03/2021 Add restart core_monitor_1 core_controller_1 in core install scripts
