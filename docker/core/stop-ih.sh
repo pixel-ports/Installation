1de9627985f32d23d84fe91e1acfe118c4058b0d
Marc Despland <marc.despland@orange.com>
2021-03-05 13:06:14 +0000

    rewrite stop-ih.sh

--- docker/core/stop-ih.sh
+++ docker/core/stop-ih.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-docker-compose -f docker-compose.yaml -f docker-compose-ih-stop.yaml down -v
+# STOP ALL IH CONTAINERS
+read -p "Stop ALL Information Hub containers ? " -n 1 -r
+echo  ""
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    docker stop core_orion-collector_1 core_monitor_1 core_writer_1 core_extractor_1 core_orchestrator_1 core_controller_1 core_elasticsearch-proxy_1 kafka zookeeper 
+    echo "done"
+fi
+
+
+# REMOVE ALL IH CONTAINERS
+read -p "Remove ALL Information Hub containers ? " -n 1 -r
+echo  ""
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    docker rm core_orion-collector_1 core_monitor_1 core_writer_1 core_extractor_1 core_orchestrator_1 core_controller_1 core_elasticsearch-proxy_1 kafka zookeeper 
+    echo "done"
+fi
