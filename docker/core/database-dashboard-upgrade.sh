a40455b50a40fc226be28c0df203adaa4fba63c3
root <root@OlpAzureVm2.tz0pgx5hu11urgo0lfyn4gkteg.ax.internal.cloudapp.net>
2021-03-09 11:28:45 +0000

    Updated dashboard to 2.1.5

---
+++ docker/core/database-dashboard-upgrade.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+source .env
+
+PORT=$(echo $PIXEL_DOMAIN| cut -d'.' -f 2)
+
+PORT=$(echo $PIXEL_DOMAIN| cut -d'.' -f 2)
+
+if [ -z "$PORT" ]
+then
+      PORT="develop"
+	  echo "Updating Dashboard database with default values"
+else
+	  echo "Updating Dashboard database for the port: $PORT"
+fi
+
+docker-compose -f docker-compose.yaml -f docker-compose-dashboard.yaml exec dashboard-api-db /bin/sh -c "/usr/bin/mongo pixel-db --eval 'db.dropDatabase()'"
+docker-compose -f docker-compose.yaml -f docker-compose-dashboard.yaml exec dashboard-api /bin/sh -c "/api/seed/insert.sh $PORT" 
+
+echo 'seed complete'
+
+exit 0
