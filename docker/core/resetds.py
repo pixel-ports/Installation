ded977ec03d1710310cba1a2230e4e2dde54b86e
Damjan Murn <damjan.murn@xlab.si>
2021-03-24 13:00:32 +0100

    Some improvements of resetds.py script
    * Add --delete-subscr switch for deleting the data source subscription in Orion
    * Remove activate subscription logic because it is handled by IH
    * Add some sleeps
    * Check if Orion subscription exists at the end

--- docker/core/resetds.py
+++ docker/core/resetds.py
@@ -5,7 +5,7 @@ import time
 import docker
 import requests
 
-VERSION = 1.0
+VERSION = 1.1
 
 """
 Works with any Python3 version that should be installed with your OS
@@ -15,7 +15,7 @@ Then you can install dependencies : pip3 install docker requests
 Run : python3 resetds.py --help
 """
 
-def main(srctype: str, srcname: str):
+def main(srctype: str, srcname: str, delete_subscr: bool):
     print("\n# Damjan's procedure to delete the source through IH API")
     print("Retrieve IH container IP address")
     client = docker.from_env()
@@ -27,14 +27,50 @@ def main(srctype: str, srcname: str):
     print("Deactivate the datasource (stop collecting data)")
     r = requests.put(baseurl, json={"collected": False, "archived": False})
     print("Success" if r.ok else f"Failed : {r.reason}")
+    time.sleep(1)
+
     print("Delete the datasource")
     r = requests.delete(baseurl)
     print("Success" if r.ok else f"Failed : {r.reason}")
+    time.sleep(1)
+
     print("Delete the source type")
     baseurl = baseurl.split("/source/")[0]
     r = requests.delete(baseurl)
     print("Success" if r.ok else f"Failed : {r.reason}")
     time.sleep(1)
+
+    if delete_subscr:
+        print(f"Looking for existing IH subscription to Orion datasource {srcname}")
+        baseurl = "http://127.0.0.1:1026/v2"
+        url = f"{baseurl}/subscriptions"
+        r = requests.get(url)
+        if r.ok:
+            print("Success")
+        else:
+            print(f"Failed : {r.reason}")
+            sys.exit(1)
+
+        subscriptions = [x for x in r.json() if x['description'] ==
+                         f"Information Hub subscription to the source {srcname}."]
+        if not subscriptions:
+            print("Existing subscription has not been found.")
+        elif len(subscriptions) > 1:
+            print("Too many subscriptions found !!!!")
+            print(subscriptions)
+            sys.exit(6)
+        else:
+            subscr = subscriptions[0]
+            subscr_id = subscr["id"]
+            print("Subscription found: " + subscr_id)
+            print("Delete the Orion subsciption")
+            subscr_id = subscr["id"]
+            r = requests.delete(f"{url}/{subscr_id}")
+            if r.ok:
+                print(f"Successfully deleted subscription {subscr_id}")
+            else:
+                print(f"Failed : {r.reason}")
+                sys.exit(1)
 
     print("\n# Marc's procedure to force detection of a datasource through Orion")
     print("Note: the datasource has been created through the DAL Orchestrator at the agent creation time")
@@ -57,58 +93,40 @@ def main(srctype: str, srcname: str):
     print("Success" if r.ok else f"Failed : {r.reason}")
     if not r.ok:
         sys.exit(4)
-    time.sleep(1)
+    time.sleep(2.5)
 
-    print("\n# Check IH process work properly")
+    print("\n# Check if datasource has been registered successfully")
     print(
         f"Check there's an IH subscription to Orion for datasource {srcname}")
     url = f"{baseurl}/subscriptions"
     r = requests.get(url)
-    print("Success" if r.ok else f"Failed : {r.reason}")
     subscriptions = [x for x in r.json() if x['description'] ==
                      f"Information Hub subscription to the source {srcname}."]
     if not subscriptions:
-        print("Subscription not found !!!!")
-        sys.exit(5)
-    if len(subscriptions) > 1:
+        print("Subscription has not been found!")
+        if delete_subscr:
+            print("Note: importing the historical data might take some time.")
+        sys.exit(1)
+    elif len(subscriptions) > 1:
         print("Too many subscriptions found !!!!")
         print(subscriptions)
         sys.exit(6)
-    subscr = subscriptions[0]
-    print("subscr = " + str(subscr))
-    if subscr["status"] != "active":
-        print("Subsciption found but not active !!!!")
-        print("Try to activate the subsciption")
-        id = subscr["id"]
-        r = requests.patch(f"{url}/{id}", json={"status": "active"})
-        print("Successful patch request" if r.ok else f"Failed : {r.reason}")
-        time.sleep(1)
-        # check ativation
-        if not r.ok:
-            sys.exit(7)
-        r = requests.get(f"{url}/{id}")
-        print("Successful get request" if r.ok else f"Failed : {r.reason}")
-        if not r.ok:
-            sys.exit(8)
-        subscr = r.json()
-        status = subscr["status"]
-        if status != "active":
-            print("Cannot activate subsciption !")
-            sys.exit(9)
-    print("subscr = " + str(subscr))
-    print(
-        "\n\033[1mSubscription found and active. It should work fine !\033[0m\n")
-    sys.exit(0)
+    else:
+        subscr = subscriptions[0]
+        print(f"Orion subscription found: {subscr['id']}. Status: {subscr['status']}")
+        if subscr['status']:
+            print(f"Datasource {srcname} has been re-registered successfully.")
 
 
 def help():
-    print("Usage : resetds <sourceType> <sourceName>")
+    print("Usage : resetds <sourceType> <sourceName> [--delete-subscr]")
     print("Usage : resetds --help")
     print("Usage : resetds --version")
     print("Chain Damjan and Marc procedures to respectively :")
     print("  - delete a datasource through IH API")
     print("  - force detection of a datasource through Orion")
-    print("Eventually check the IH subscription")
+    print("Use --delete-subscr switch to delete data source subscription in Orion. Consequently, the IH will reimport ")
+    print("historical data from Orion when data source is re-registered in IH. Use this switch if you have also deleted data source index in Elasticsearch.")
     print("Use this script when data are stored in Orion but doesn't reach the Information Hub")
     print("Example : resetds AirQualityObserved urn:pixel:DataSource:frbod:AtmoNouvelleAquitaine")
 
@@ -116,17 +134,21 @@ def help():
 if __name__ == "__main__":
     opts = [opt for opt in sys.argv[1:] if opt.startswith("-")]
     args = [arg for arg in sys.argv[1:] if not arg.startswith("-")]
+    delete_subscr = False
+
     if opts:
         if opts[0] == "-h" or opts[0] == "--help":
             help()
             sys.exit(0)
         if opts[0] == "-V" or opts[0] == "--version":
             print(f"{sys.argv[0]} version {VERSION}")
-            sys.exit(0)            
+            sys.exit(0)
+        if "--delete-subscr" in opts:
+            delete_subscr = True
     if not args or len(args) != 2:
         help()
         sys.exit(1)
     if not args[1].startswith("urn:"):
         print("Invalid source name. Source names MUST begin with 'urn:'")
         sys.exit(2)
-    main(*args)
+    main(*args, delete_subscr)
