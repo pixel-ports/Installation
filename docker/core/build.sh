fe28b00cec0f17299eef3e72c572eb4d46941cbf
benmomo <benmomo@upvnet.upv.es>
2021-05-05 07:17:08 +0000

    Update 'docker/core/build.sh'

--- docker/core/build.sh
+++ docker/core/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+mkdir -p /opt/pixel/data/filesUp
 cd dashboard; ./configure.sh; cd ..
 cd ot-core; ./configure.sh; cd ..
 docker-compose -f docker-compose.yaml -f docker-compose-security.yaml -f docker-compose-elastic.yaml -f docker-compose-ih.yaml -f docker-compose-tools.yaml -f docker-compose-ot.yaml -f docker-compose-dashboard.yaml build
